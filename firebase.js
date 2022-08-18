880674dc47e4cbc2afbf13fc79a007670b2eee73
BlueZhiaar <tightblackmachine@gmail.com>
2022-08-18 11:43:00 +0900

    firebaseアプリの初期設定を追加

---
+++ firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC6Q1UpeXpWKDs3PlcKiz0qeGSQWhUQdzE",
+  authDomain: "bluezhiaar-diary.firebaseapp.com",
+  projectId: "bluezhiaar-diary",
+  storageBucket: "bluezhiaar-diary.appspot.com",
+  messagingSenderId: "1093963449209",
+  appId: "1:1093963449209:web:667a1d8e9818b09b1025a8",
+  measurementId: "G-Z1V483VH0Z"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+if(typeof window !== 'undefined'){
+    const analytics = getAnalytics(app);
+}
